333ab015c2134ffa0f9e7220e24bb791bea4cb8b
tarekabdallah5756 <dmirt576@gmail.com>
2025-04-17 21:55:22 +0200

    Prep for Replit: use $PORT, add requirements

--- flaskk.py
+++ flaskk.py
@@ -47,7 +47,10 @@ def predict():
     return render_template('index.html', predictions=predictions)
 
 if __name__ == '__main__':
-    app.run(port=3000, debug=True)
+    import os
+    port = int(os.environ.get('PORT', 3000))
+    app.run(host='0.0.0.0', port=port, debug=True)
+
 
 
 
